061eb732b0feb558aa060a3e2d750a7db8e176dc
matheusa1 <2002matheus@gmail.com>
2021-10-28 00:06:37 -0400

    Atualizando

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,13 +1,31 @@
 const Database = require('../db/config')
 
 module.exports = {
-    index(req, res) {
+    async index(req, res) {
+        const db = await Database() 
+
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
 
-        console.log(`room = ${roomId}, question = ${questionId}, action = ${action}, password = ${password}`);
+        
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(verifyRoom.pass == password) {
+            if(action == "delete") {
+
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+
+            } else if(action == "check") {
+
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+
+            }
+        }
+
+        await db.close(``)
+
+        res.redirect(`/room/${roomId}`)
     },
 
     async create(req, res) {
@@ -24,6 +42,8 @@ module.exports = {
             ${roomId},
             0
         )`)
+
+        await db.close(``)
 
         res.redirect(`/room/${roomId}`)
     }
